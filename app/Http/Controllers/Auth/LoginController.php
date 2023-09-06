a7a67cc6eb6a1437cfb2a27849d8924119f0f738
Jun <xjunsantos@gmail.com>
2023-09-06 18:49:27 +0800

    Authorized request

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -13,9 +13,15 @@ class LoginController extends Controller
 {
     public function __invoke(LoginRequest $request)
     {
-        $user = User::where('email', $request->email)->first();
+        // $user = User::where('email', $request->email)->first();
 
-        if(!$user || !Hash::check($request->password, $user->password)) {
+        // if(!$user || !Hash::check($request->password, $user->password)) {
+        //     throw ValidationException::withMessages([
+        //         'email' => ['The credential you enter is incorrect.']
+        //     ]);
+        // }
+
+        if(!auth()->attempt($request->only(['email', 'password']))) {
             throw ValidationException::withMessages([
                 'email' => ['The credential you enter is incorrect.']
             ]);
