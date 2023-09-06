c5c85c5301ac212dac36b93290aa565140c00326
Jun <xjunsantos@gmail.com>
2023-09-06 19:25:47 +0800

    API Token Authentication

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The credential you enter is incorrect.']
+            ]);
+        }
+
+        // if(!auth()->attempt($request->only(['email', 'password']))) {
+        //     throw ValidationException::withMessages([
+        //         'email' => ['The credential you enter is incorrect.']
+        //     ]);
+        // }
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('api_token')->plainTextToken
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 
 /*
@@ -23,4 +26,10 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function() {
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('auth')->group(function() {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
 });
