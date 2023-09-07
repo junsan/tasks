b49840ad294b229d386cb287d07a4385828856f9
Jun Santos <xjunsantos@gmail.com>
2023-09-07 09:04:05 +0800

    User relationship and V2 API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,7 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        Task::factory(10)->create();
+        
+        User::factory(5)->has(Task::factory(5))->create();
     }
 }
